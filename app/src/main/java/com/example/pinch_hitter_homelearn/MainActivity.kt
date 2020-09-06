9f753fea6c27f66dfeebb9b886d6f66db06eda5a
WooSangGyu <wsg0701@naver.com>
2020-09-06 15:08:33 +0900

    first commit

---
+++ app/src/main/java/com/example/pinch_hitter_homelearn/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pinch_hitter_homelearn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
